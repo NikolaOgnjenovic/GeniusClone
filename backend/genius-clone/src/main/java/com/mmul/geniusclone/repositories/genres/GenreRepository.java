684dd24da153145e465a0441c3c1edcf6cce0dcd
Luka Bradic <bradicluka04@gmail.com>
2024-06-26 23:44:52 +0200

    Add repository

--- backend/genius-clone/src/main/java/com/mmul/geniusclone/repositories/genres/GenreRepository.java
+++ backend/genius-clone/src/main/java/com/mmul/geniusclone/repositories/genres/GenreRepository.java
@@ -1,2 +1,10 @@
-package com.mmul.geniusclone.repositories.genres;public class GenresRepository {
+package com.mmul.geniusclone.repositories.genres;
+
+import com.mmul.geniusclone.models.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface GenreRepository extends JpaRepository<Genre, UUID> {
+
 }
