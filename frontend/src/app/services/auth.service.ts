c87269da8b86398c91aeaaa8b397b8a12a7cf3c2
Luka Bradic <bradicluka04@gmail.com>
2024-06-26 20:18:28 +0200

    Add login page to frontend

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {RegistrationRequest} from "../models/auth/registration-request";
 import {RegistrationResponse} from "../models/auth/registration-response";
+import {LoginRequest} from "../models/auth/login-request";
+import {LoginResponse} from "../models/auth/login-response";
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +16,9 @@ export class AuthService {
 
   register(request: RegistrationRequest): Observable<RegistrationResponse> {
     return this.http.post<RegistrationResponse>(`${this.baseUrl}/register`, request);
+  }
+
+  login(request: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, request);
   }
 }
