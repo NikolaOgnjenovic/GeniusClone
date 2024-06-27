7b02305702f0301d50280db75222b99f6d7cacb5
Luka Bradic <bradicluka04@gmail.com>
2024-06-27 16:46:10 +0200

    Fix ReviewRepository

--- backend/genius-clone/src/main/java/com/mmul/geniusclone/repositories/reviews/ReviewRepository.java
+++ backend/genius-clone/src/main/java/com/mmul/geniusclone/repositories/reviews/ReviewRepository.java
@@ -2,9 +2,11 @@ package com.mmul.geniusclone.repositories.reviews;
 
 import com.mmul.geniusclone.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
 }
